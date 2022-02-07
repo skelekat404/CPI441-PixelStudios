f555f2efad1fed25faecd8c8759c2a08c9c6a7d1
Morgan Cividanes <morgancivida@gmail.com>
2022-02-07 16:22:00 -0700

    added sprint cooldown to earth scene

--- Assets/Scripts/Motion/On_Planet_Movement.cs
+++ Assets/Scripts/Motion/On_Planet_Movement.cs
@@ -10,6 +10,14 @@ public class On_Planet_Movement : MonoBehaviour
     float curr_passive_speed;
     //Vector3 forward2D = new Vector3(1, 0, 0);
     // Start is called before the first frame update
+
+    //Sprint cooldown variables
+    public float sprintSpeed = 2f;
+    public float sprintLength = 0.5f;
+    public float sprintCoolDown = 120f;
+
+    private float sprintCounter;
+    private float sprintCoolCounter;
     void Start()
     {
         //active_speed = 5;
@@ -43,12 +51,36 @@ public class On_Planet_Movement : MonoBehaviour
             }
             else if ((Input.GetKey("up") || Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
             {
-                gameObject.transform.position += gameObject.transform.right * active_speed * 1.1f;
-                curr_passive_speed = passive_speed;
+                if (sprintCoolCounter <= 0 && sprintCounter <= 0)
+                {
+                    //Update this variable for faster/slower accelerate speed
+                    active_speed = 0.04f;
+                    gameObject.transform.position += gameObject.transform.right * active_speed;
+                    curr_passive_speed = passive_speed;
+
+                    sprintCounter = sprintLength;
+                }
+
+                
+            }
+            if (sprintCounter > 0)
+            {
+                sprintCounter -= Time.deltaTime;
+                if (sprintCounter <= 0)
+                {
+                    //Reset speed back to default
+                    active_speed = 0.009f;
+                    sprintCoolCounter = sprintCoolDown;
+                }
             }
 
-            //apply passive speed
-            gameObject.transform.position += gameObject.transform.right * curr_passive_speed;
+            if (sprintCoolCounter > 0)
+            {
+                sprintCoolCounter -= Time.deltaTime;
+            }
+
+        //apply passive speed
+        gameObject.transform.position += gameObject.transform.right * curr_passive_speed;
         
 
 
