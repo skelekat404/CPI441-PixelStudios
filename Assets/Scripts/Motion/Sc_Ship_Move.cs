6cac2e1adc7f9cef2300b31bb83d848994268722
Alex <alexandrehcayer@gmail.com>
2022-02-07 23:55:48 -0700

    Revert "Merge branch 'main' into alex-branch"
    This reverts commit 4faba220d472497d6244741478114747e54fb9ea.

--- Assets/Scripts/Motion/Sc_Ship_Move.cs
+++ Assets/Scripts/Motion/Sc_Ship_Move.cs
@@ -11,15 +11,6 @@ public class Sc_Ship_Move : NetworkBehaviour
     float curr_passive_speed;
     //Vector3 forward2D = new Vector3(1, 0, 0);
     // Start is called before the first frame update
-
-    //Sprint cooldown variables
-    public float sprintSpeed = 2f;
-    public float sprintLength = 0.5f;
-    public float sprintCoolDown = 120f;
-
-    private float sprintCounter;
-    private float sprintCoolCounter;
-
     void Start()
     {
         //active_speed = 5;
@@ -34,58 +25,26 @@ public class Sc_Ship_Move : NetworkBehaviour
         if(IsLocalPlayer)
         {
             //moving
-            if (Input.GetKey("up") || Input.GetKey(KeyCode.W))
+            if (Input.GetKey("up"))
             {
                 gameObject.transform.position += gameObject.transform.right * active_speed;
                 curr_passive_speed = passive_speed;
             }
-            else if (Input.GetKeyDown("down") || Input.GetKey(KeyCode.S))
+            else if (Input.GetKeyDown("down"))
             {
                 curr_passive_speed = 0;
             }
             //turning
-            if (Input.GetKey("left") || Input.GetKey(KeyCode.A))
+            if (Input.GetKey("left"))
             {
                 gameObject.transform.eulerAngles += gameObject.transform.forward * degrees * Time.deltaTime;
             }
-            else if (Input.GetKey("right") || Input.GetKey(KeyCode.D))
+            else if (Input.GetKey("right"))
             {
                 gameObject.transform.eulerAngles += gameObject.transform.forward * -degrees * Time.deltaTime;
             }
-            else if((Input.GetKey("up") || Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
-            {
-                if(sprintCoolCounter <= 0 && sprintCounter <= 0)
-                {
-                    //Update this variable for faster/slower accelerate speed
-                    active_speed = 0.04f;
-                    gameObject.transform.position += gameObject.transform.right * active_speed;
-                    curr_passive_speed = passive_speed;
-
-                    sprintCounter = sprintLength;
-                }
-                
-            }
-
-            if(sprintCounter > 0)
-            {
-                sprintCounter -= Time.deltaTime;
-                if(sprintCounter <= 0)
-                {
-                    //Reset speed back to default
-                    active_speed = 0.009f;
-                    sprintCoolCounter = sprintCoolDown;
-                }
-            }
-
-            if(sprintCoolCounter > 0)
-            {
-                sprintCoolCounter -= Time.deltaTime;
-            }
-
             //apply passive speed
             gameObject.transform.position += gameObject.transform.right * curr_passive_speed;
         }
-
-        
     }
 }
