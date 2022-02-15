e229eed9cf91614e7fd04bc839228f9af4c81643
A-man624 <armart71@asu.edu>
2022-02-14 18:23:18 -0700

    Added basic material harvesting and Important Variables script

---
+++ Assets/Scripts/SCR_ImportantVariables.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_ImportantVariables : MonoBehaviour
+{
+    public int numWood = 0;
+    public int numRock = 0;
+}
