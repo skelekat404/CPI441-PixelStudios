2d2d90eb32b2a831bdffcb0954a666d7ab6468ad
Nick Walker <njwalke4@asu.edu>
2022-02-15 15:12:57 -0700

    Add inventory to planet, minor changes
    Added inventory to planet, CHANGED inventory bind to "E", can press "B" to spawn beans in inventory

--- Assets/ToggleInventory.cs
+++ Assets/ToggleInventory.cs
@@ -6,9 +6,13 @@ public class ToggleInventory : MonoBehaviour
 {
     public bool inventoryToggle = false;
     [SerializeField]
+    private InventoryChannel InventoryChannel;
+    [SerializeField]
     private InventoryUIChannel InventoryUIChannel;
     [SerializeField]
     private InventoryHolder inventoryHolder;
+    [SerializeField]
+    private InventorySystem.InventoryItem itemToGet;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +22,14 @@ public class ToggleInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        if(Input.GetKeyDown(KeyCode.E))
         {
             inventoryToggle = !inventoryToggle;
             InventoryUIChannel.RaiseToggle(inventoryHolder);
+        }
+        if(Input.GetKeyDown(KeyCode.B)) //Press B to generate beans -- meant as a test for "button press to get items"
+        {
+            InventoryChannel?.RaiseLootItem(itemToGet);
         }
     }
 }
