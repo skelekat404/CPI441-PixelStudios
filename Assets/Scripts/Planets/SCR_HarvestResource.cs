27a7c5fc51cb91a5968415e2735fab3b5d419d3c
Nick Walker <njwalke4@asu.edu>
2022-02-24 10:10:14 -0700

    joined inventory & resource harvesting
    Made changes to SCR_HarvestResource, edited a few item types, assigned tiles to their respective resources

--- Assets/Scripts/Planets/SCR_HarvestResource.cs
+++ Assets/Scripts/Planets/SCR_HarvestResource.cs
@@ -8,6 +8,13 @@ public class SCR_HarvestResource : MonoBehaviour
     public int currentHits = 0;
     public int maxHits = 3;
 
+    // *** Identifies which inventory looted items go to ***
+    [SerializeField]
+    private InventoryChannel m_InventoryChannel;
+    // *** Identifies which item is to be looted ***
+    [SerializeField]
+    private InventorySystem.InventoryItem m_LootableItem;
+
     // *** Used to reference variables file with material counts ***
     public SCR_ImportantVariables importantVariables;
 
@@ -56,12 +63,14 @@ public class SCR_HarvestResource : MonoBehaviour
                 importantVariables.numWood++;
                 Destroy(this.gameObject);
                 Debug.Log("Wood #: " + importantVariables.numWood);
+                m_InventoryChannel?.RaiseLootItem(m_LootableItem);
             }
             if (gameObject.tag == "Rock")
             {
                 importantVariables.numRock++;
                 Destroy(this.gameObject);
                 Debug.Log("Rock #: " + importantVariables.numRock);
+                m_InventoryChannel?.RaiseLootItem(m_LootableItem);
             }
         }
     }
