1fdd4423a484e8dab0c9bcb9c346f36a647a248c
Alex <alexandrehcayer@gmail.com>
2022-03-15 15:08:00 -0700

    orange scene transition and hud fix
    i deleted the meta file this time :D

---
+++ Assets/Scripts/UI_Managers/orange_bandaid.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orange_bandaid : MonoBehaviour
+{
+    private GameObject _target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_target == null)
+        {
+            if (GameObject.FindGameObjectWithTag("GameController") != null)
+            {
+                _target = GameObject.FindGameObjectWithTag("orangeBandaid");
+            }
+        }
+        else
+        {
+            _target.SetActive(false);
+        }
+    }
+}
